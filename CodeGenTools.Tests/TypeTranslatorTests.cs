a2280afbc4b8f287f1797564094c1399e3bf55d3
chrcar01 <chrisjcarter@gmail.com>
2011-03-31 19:55:52 -0600

    initial files

---
+++ CodeGenTools.Tests/TypeTranslatorTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace CodeGenTools.Tests
+{
+	[TestFixture]
+	public class TypeTranslatorTests
+	{
+		[Test]
+		public void CanConvertSqlServerTypeToSqlDbType()
+		{
+			Assert.AreEqual(SqlDbType.BigInt, TypeTranslator.ToSqlDbType("bigint"));
+			Assert.AreEqual(SqlDbType.Binary, TypeTranslator.ToSqlDbType("binary"));
+			Assert.AreEqual(SqlDbType.Bit, TypeTranslator.ToSqlDbType("bit"));
+			Assert.AreEqual(SqlDbType.Char, TypeTranslator.ToSqlDbType("char"));
+			Assert.AreEqual(SqlDbType.Date, TypeTranslator.ToSqlDbType("date"));
+			Assert.AreEqual(SqlDbType.DateTime, TypeTranslator.ToSqlDbType("datetime"));
+			Assert.AreEqual(SqlDbType.DateTime2, TypeTranslator.ToSqlDbType("datetime2"));
+			Assert.AreEqual(SqlDbType.DateTimeOffset, TypeTranslator.ToSqlDbType("datetimeoffset"));
+			Assert.AreEqual(SqlDbType.Decimal, TypeTranslator.ToSqlDbType("decimal"));
+			Assert.AreEqual(SqlDbType.Float, TypeTranslator.ToSqlDbType("float"));
+			Assert.AreEqual(SqlDbType.Image, TypeTranslator.ToSqlDbType("image"));
+			Assert.AreEqual(SqlDbType.Int, TypeTranslator.ToSqlDbType("int"));
+			Assert.AreEqual(SqlDbType.Money, TypeTranslator.ToSqlDbType("money"));
+			Assert.AreEqual(SqlDbType.NChar, TypeTranslator.ToSqlDbType("nchar"));
+			Assert.AreEqual(SqlDbType.NText, TypeTranslator.ToSqlDbType("ntext"));
+			Assert.AreEqual(SqlDbType.Decimal, TypeTranslator.ToSqlDbType("numeric"));
+			Assert.AreEqual(SqlDbType.NVarChar, TypeTranslator.ToSqlDbType("nvarchar"));
+			Assert.AreEqual(SqlDbType.SmallDateTime, TypeTranslator.ToSqlDbType("smalldatetime"));
+			Assert.AreEqual(SqlDbType.SmallInt, TypeTranslator.ToSqlDbType("smallint"));
+			Assert.AreEqual(SqlDbType.SmallMoney, TypeTranslator.ToSqlDbType("smallmoney"));
+			Assert.AreEqual(SqlDbType.Variant, TypeTranslator.ToSqlDbType("sql_variant"));
+			Assert.AreEqual(SqlDbType.Text, TypeTranslator.ToSqlDbType("text"));
+			Assert.AreEqual(SqlDbType.Time, TypeTranslator.ToSqlDbType("time"));
+			Assert.AreEqual(SqlDbType.Timestamp, TypeTranslator.ToSqlDbType("timestamp"));
+			Assert.AreEqual(SqlDbType.TinyInt, TypeTranslator.ToSqlDbType("tinyint"));
+			Assert.AreEqual(SqlDbType.UniqueIdentifier, TypeTranslator.ToSqlDbType("uniqueidentifier"));
+			Assert.AreEqual(SqlDbType.VarBinary, TypeTranslator.ToSqlDbType("varbinary"));
+			Assert.AreEqual(SqlDbType.VarChar, TypeTranslator.ToSqlDbType("varchar"));
+			Assert.AreEqual(SqlDbType.Xml, TypeTranslator.ToSqlDbType("xml"));
+		}
+		[Test]
+		public void CanConvertSqlServerTypeToCSharpAlias()
+		{
+			Assert.AreEqual("long", TypeTranslator.ToCSharpAlias("bigint"));
+			Assert.AreEqual("byte[]", TypeTranslator.ToCSharpAlias("binary"));
+			Assert.AreEqual("bool", TypeTranslator.ToCSharpAlias("bit"));
+			Assert.AreEqual("string", TypeTranslator.ToCSharpAlias("char"));
+			Assert.AreEqual("System.DateTime", TypeTranslator.ToCSharpAlias("date"));
+			Assert.AreEqual("System.DateTime", TypeTranslator.ToCSharpAlias("datetime"));
+			Assert.AreEqual("System.DateTime", TypeTranslator.ToCSharpAlias("datetime2"));
+			Assert.AreEqual("decimal", TypeTranslator.ToCSharpAlias("decimal"));
+			Assert.AreEqual("double", TypeTranslator.ToCSharpAlias("float"));
+			Assert.AreEqual("byte[]", TypeTranslator.ToCSharpAlias("image"));
+			Assert.AreEqual("int", TypeTranslator.ToCSharpAlias("int"));
+			Assert.AreEqual("double", TypeTranslator.ToCSharpAlias("money"));
+			Assert.AreEqual("string", TypeTranslator.ToCSharpAlias("nchar"));
+			Assert.AreEqual("string", TypeTranslator.ToCSharpAlias("ntext"));
+			Assert.AreEqual("double", TypeTranslator.ToCSharpAlias("numeric"));
+			Assert.AreEqual("string", TypeTranslator.ToCSharpAlias("nvarchar"));
+			Assert.AreEqual("System.DateTime", TypeTranslator.ToCSharpAlias("smalldatetime"));
+			Assert.AreEqual("short", TypeTranslator.ToCSharpAlias("smallint"));
+			Assert.AreEqual("double", TypeTranslator.ToCSharpAlias("smallmoney"));
+			Assert.AreEqual("object", TypeTranslator.ToCSharpAlias("sql_variant"));
+			Assert.AreEqual("string", TypeTranslator.ToCSharpAlias("text"));
+			Assert.AreEqual("System.DateTime", TypeTranslator.ToCSharpAlias("time"));
+			Assert.AreEqual("byte[]", TypeTranslator.ToCSharpAlias("timestamp"));
+			Assert.AreEqual("byte", TypeTranslator.ToCSharpAlias("tinyint"));
+			Assert.AreEqual("System.Guid", TypeTranslator.ToCSharpAlias("uniqueidentifier"));
+			Assert.AreEqual("byte[]", TypeTranslator.ToCSharpAlias("varbinary"));
+			Assert.AreEqual("string", TypeTranslator.ToCSharpAlias("varchar"));
+			Assert.AreEqual("string", TypeTranslator.ToCSharpAlias("xml"));
+		}
+		[Test]
+		public void CaseInsensitiveDictionaryTests()
+		{
+			var dict = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+			dict.Add("test", "test");
+			Assert.IsTrue(dict.ContainsKey("TeSt"));
+		}
+	}
+}
---
+++ CodeGenTools/TypeTranslator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+
+namespace CodeGenTools
+{
+	public static class TypeTranslator
+	{
+		public static string ToClrTypeName(string sqlServerDataType)
+		{
+			throw new NotImplementedException();
+		}
+		public static string ToCSharpAlias(string sqlServerDataType)
+		{
+			var dict = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+			dict.Add("bigint", "long");
+			dict.Add("binary", "byte[]");
+			dict.Add("bit", "bool");
+			dict.Add("char", "string");
+			dict.Add("date", "System.DateTime");
+			dict.Add("datetime", "System.DateTime");
+			dict.Add("datetime2", "System.DateTime");
+			dict.Add("decimal", "decimal");
+			dict.Add("float", "double");
+			dict.Add("image", "byte[]");
+			dict.Add("int", "int");
+			dict.Add("money", "double");
+			dict.Add("nchar", "string");
+			dict.Add("ntext", "string");
+			dict.Add("numeric", "double");
+			dict.Add("nvarchar", "string");
+			dict.Add("smalldatetime", "System.DateTime");
+			dict.Add("smallint", "short");
+			dict.Add("smallmoney", "double");
+			dict.Add("sql_variant", "object");
+			dict.Add("text", "string");
+			dict.Add("time", "System.DateTime");
+			dict.Add("timestamp", "byte[]");
+			dict.Add("tinyint", "byte");
+			dict.Add("uniqueidentifier", "System.Guid");
+			dict.Add("varbinary", "byte[]");
+			dict.Add("varchar", "string");
+			dict.Add("xml", "string");
+			if (!dict.ContainsKey(sqlServerDataType))
+				throw new KeyNotFoundException(sqlServerDataType + " is not mapped.");
+
+			return dict[sqlServerDataType];
+		}
+		public static SqlDbType ToSqlDbType(string sqlServerDataType)
+		{
+			var dict = new Dictionary<string, SqlDbType>(StringComparer.OrdinalIgnoreCase);
+			dict.Add("bigint", SqlDbType.BigInt);
+			dict.Add("binary", SqlDbType.Binary);
+			dict.Add("bit", SqlDbType.Bit);
+			dict.Add("char", SqlDbType.Char);
+			dict.Add("date", SqlDbType.Date);
+			dict.Add("datetime", SqlDbType.DateTime);
+			dict.Add("datetime2", SqlDbType.DateTime2);
+			dict.Add("datetimeoffset", SqlDbType.DateTimeOffset);
+			dict.Add("decimal", SqlDbType.Decimal);
+			dict.Add("float", SqlDbType.Float);
+			dict.Add("image", SqlDbType.Image);
+			dict.Add("int", SqlDbType.Int);
+			dict.Add("money", SqlDbType.Money);
+			dict.Add("nchar", SqlDbType.NChar);
+			dict.Add("ntext", SqlDbType.NText);
+			dict.Add("numeric", SqlDbType.Decimal);
+			dict.Add("nvarchar", SqlDbType.NVarChar);
+			dict.Add("smalldatetime", SqlDbType.SmallDateTime);
+			dict.Add("smallint", SqlDbType.SmallInt);
+			dict.Add("smallmoney", SqlDbType.SmallMoney);
+			dict.Add("sql_variant", SqlDbType.Variant);
+			dict.Add("text", SqlDbType.Text);
+			dict.Add("time", SqlDbType.Time);
+			dict.Add("timestamp", SqlDbType.Timestamp);
+			dict.Add("tinyint", SqlDbType.TinyInt);
+			dict.Add("uniqueidentifier", SqlDbType.UniqueIdentifier);
+			dict.Add("varbinary", SqlDbType.VarBinary);
+			dict.Add("varchar", SqlDbType.VarChar);
+			dict.Add("xml", SqlDbType.Xml);
+
+			if (!dict.ContainsKey(sqlServerDataType))
+				throw new KeyNotFoundException(sqlServerDataType + " is not mapped.");
+
+			return dict[sqlServerDataType];
+		}
+	}
+}
